09bc018cd537104e2c5210e303f4530034511eb8
Clement Tagoe <clementtagoe110@gmail.com>
2021-03-03 15:02:20 +0000

    Add about and academics page

--- script.js
+++ script.js
@@ -7,6 +7,15 @@ setInterval(function() {
     }
 }, 5000);
 
+var counter2 = 1;
+setInterval(function() {
+    document.getElementById('a-radio' + counter2).checked = true;
+    counter2++;
+    if(counter2 > 4) {
+        counter2 = 1;
+    }
+}, 5000);
+
 
 const toggle = document.querySelector('.toggle');
 const navbar = document.querySelector('.navbar nav');
